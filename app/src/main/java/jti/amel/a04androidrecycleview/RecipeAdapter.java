e22e5fadf8fdd5c1f550dd6f22cb6f50f6d602f2
amelia <meliamel.9977@gmail.com>
2022-09-28 23:35:57 +0700

    homework

---
+++ app/src/main/java/jti/amel/a04androidrecycleview/RecipeAdapter.java
@@ -0,0 +1,86 @@
+package jti.amel.a04androidrecycleview;
+
+import android.app.ActionBar;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder> {
+    private ArrayList<String> namaMakanan = new ArrayList<>();
+    private ArrayList<String> descMakanan = new ArrayList<>();
+    private ArrayList<String> fotoMakanan = new ArrayList<>();
+    private ArrayList<String> bahanMakanan = new ArrayList<>();
+    private ArrayList<String> caraMembuat = new ArrayList<>();
+    private Context context;
+    private View.OnClickListener mOnClick;
+
+    public RecipeAdapter(ArrayList<String> namaMakanan, ArrayList<String> descMakanan, ArrayList<String> fotoMakanan, ArrayList<String> bahanMakanan, ArrayList<String> caraMembuat, Context context) {
+        this.namaMakanan = namaMakanan;
+        this.descMakanan = descMakanan;
+        this.fotoMakanan = fotoMakanan;
+        this.bahanMakanan = bahanMakanan;
+        this.caraMembuat = caraMembuat;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_design_adapter,parent,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.recipeMenu.setText(namaMakanan.get(position));
+        holder.recipeDescription.setText(descMakanan.get(position));
+        holder.constraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                intent.putExtra("nama_makanan", namaMakanan.get(position));
+                intent.putExtra("foto_makanan", fotoMakanan.get(position));
+                intent.putExtra("bahan_makanan", bahanMakanan.get(position));
+                intent.putExtra("cara_membuat", caraMembuat.get(position));
+
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return namaMakanan.size();
+    }
+
+    public void setmOnClick(View.OnClickListener onItemClickListener){
+        mOnClick = onItemClickListener;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView recipeMenu;
+        TextView recipeDescription;
+        ConstraintLayout constraintLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            recipeMenu = itemView.findViewById(R.id.recipeMenu);
+            recipeDescription = itemView.findViewById(R.id.recipeDescription);
+            constraintLayout = itemView.findViewById(R.id.constraintLayout);
+            itemView.setOnClickListener(mOnClick);
+        }
+    }
+
+}
