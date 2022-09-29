77cb04b688bb1f3513ef359b37da60f5dfa2bb27
amelia <meliamel.9977@gmail.com>
2022-09-29 20:38:00 +0700

    homework update: up button

--- app/src/main/java/jti/amel/a04androidrecycleview/DetailActivity.java
+++ app/src/main/java/jti/amel/a04androidrecycleview/DetailActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
@@ -16,6 +17,10 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeButtonEnabled(true);
+        ab.setDisplayHomeAsUpEnabled(true);
 
         imageView = findViewById(R.id.imageView);
         recipeTitle = findViewById(R.id.recipeTitle);
